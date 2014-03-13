d15d67faa158394d7c43978388cd23ab60bb1841
jmdauz <jilesione@gmail.com>
2014-03-13 14:10:04 -0400

    asdf

---
+++ J_Gadget_Classes.h
@@ -0,0 +1,61 @@
+#ifndef J_GADGET_CLASSES_H
+#define J_GADGET_CLASSES_H
+
+#include <J_Fwd_Decl.h>
+//
+#include <algorithm>
+//
+#include <functional>
+//
+#include <j_tree.h>
+//
+#include <initializer_list>
+namespace jomike{
+
+template<typename Val_t, typename Comp_t = std::less<Val_t>>
+class Delimiter_Handler{
+public:
+	template<typename Iter>
+	Delimiter_Handler(Iter, Iter);
+	bool is_delim(Val_t)const;
+	void set_delim_status(const Val_t& irk_val, bool i_status);
+
+	template<typename St>
+	Delimiter_Handler(std::initializer_list<St> i_list);
+private:
+	j_tree<Val_t, Val_t, Comp_t> M_delims;
+};
+
+template<typename Val_t, typename Comp_t /*= std::less<Val_t>*/>
+template<typename St>
+Delimiter_Handler<Val_t, Comp_t>::Delimiter_Handler(std::initializer_list<St> i_list)
+	: Delimiter_Handler(i_list.begin(), i_list.end()){}
+
+template<typename Val_t, typename Comp_t>
+bool Delimiter_Handler<Val_t, Comp_t>::is_delim(Val_t i_val)const{
+	return M_delims.end() != M_delims.find(i_val);
+}
+
+
+template<typename Val_t, typename Comp_t>
+void Delimiter_Handler<Val_t, Comp_t>::set_delim_status(const Val_t& irk_val, bool i_status){
+	if(i_status){
+		M_delims.insert(irk_val);
+	} else{
+		M_delims.erase(irk_val);
+	}
+}
+
+template<typename Val_t, typename Comp_t>
+template<typename Iter>
+Delimiter_Handler<Val_t, Comp_t>::Delimiter_Handler(Iter i_delim_begin, Iter i_delim_end){
+	std::for_each(i_delim_begin, i_delim_end
+		, std::bind(&Delimiter_Handler<Val_t>::set_delim_status
+			, this, std::placeholders::_1, true));
+
+}
+
+}
+
+#endif //J_GADGET_CLASSES_H
+
